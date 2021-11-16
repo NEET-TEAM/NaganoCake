4993061270ac379f1f4b8ef9293f6da67be9462e
ojnoh <jusinyou123@gmail.com>
2021-11-16 11:15:56 +0000

    customersのshowのviewとcontrollerの作成

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,2 +1,23 @@
 class Customer::CustomersController < ApplicationController
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(user_params)
+    redirect_to customer_path(@customer.id)
+  end
+
+  private
+  def user_params
+    params.require(:customer)permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
+  end
+
+
 end
