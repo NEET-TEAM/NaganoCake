38e59b70f554803c02151f5ee7cf8eba9653b1aa
sekai5108 <sekai.5108@gmail.com>
2021-11-16 09:35:46 +0000

    Add itemsのmodel作成とdb:migrate済み

---
+++ db/migrate/20211116093410_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.string :image_id
+      t.text :explanation
+      t.integer :price
+      t.boolean :sold_out
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps
+    end
+  end
+end
