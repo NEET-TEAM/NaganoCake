136aa25a773f37e3fd694a6ae193ee104704c05b
sanotakumi <t.sano.19941118@gmail.com>
2021-11-16 01:15:27 +0000

    session_controllerのカリキュラム部分完成

--- app/controllers/customer/sessions_controller.rb
+++ app/controllers/customer/sessions_controller.rb
@@ -2,6 +2,8 @@
 
 class Customer::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  #before_actionでcustomer/sessions_controller の create アクションが実行される前に確認
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -19,16 +21,17 @@ class Customer::SessionsController < Devise::SessionsController
   # end
 
   # protected
-  
+
   # 退会しているかを判断するメソッド
   def customer_state
-    ## 【処理内容1】 入力されたemailからアカウントを1件取得
-    @customer = Customer.find_by(email: params[:customer][:email])
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得　downcaseでで大文字を小文字にすべて変換させて、大文字小文字関係なく、メールアドレス自体が合っていたらok
+    @customer = Customer.find_by(email: params[:customer][:email].downcase)
     ## アカウントを取得できなかった場合、このメソッドを終了する
     return if !@customer
-    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
-    if @customer.valid_password?(params[:customer][:password])
-      ## 【処理内容3】
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別 ＆　active_for_authentication?でcustomerのis_deleatedがfalseかどうか
+    if @customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false)
+    ## 【処理内容3】 is_deleteがtrueの場合、退会済なのでログイン画面にリダイレクト
+    　redirect_to new_customer_session_path
     end
   end
 
