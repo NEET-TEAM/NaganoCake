049e9e251a0743f3cfa7bdf430ed28eec3abdd86
EC2 Default User <ec2-user@ip-172-31-6-236.ec2.internal>
2021-11-17 04:55:02 +0000

    railsのversionup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211115231415) do
+ActiveRecord::Schema.define(version: 20211117022550) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20211115231415) do
     t.boolean  "is_deleted",             default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "create_at"
+    t.datetime "updated_at", null: false
+    t.datetime "created_at", null: false
   end
 
 end
