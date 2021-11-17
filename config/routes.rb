692c879ee2efffe5044798cfde445c4f1b810c47
EC2 Default User <ec2-user@ip-172-31-6-236.ec2.internal>
2021-11-17 06:37:43 +0000

    genreのindexとedit機能を追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   registrations: "customer/registrations",
   sessions: 'customer/sessions'
   }
+  
+  resources :genres, only:[:create, :index, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
