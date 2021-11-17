c4a26474ed758767ef4b7c9eb0357a4529f63f3b
sanotakumi <t.sano.19941118@gmail.com>
2021-11-17 04:49:53 +0000

    first commit

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,2 +1,35 @@
 class Customer::CustomersController < ApplicationController
+  
+  before_action :authenticate_customer!
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if current_customer.update(customer_params)
+      redirect_to customers_path
+    else
+      render "edit"
+    end
+  end
+
+
+  def withdraw
+    #statusのみをtrueにアップデート
+    current_customer.update(status: true)
+    redirect_to root_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :post_code, :address, :phone_number, :email,)
+
+  end
+  
 end
