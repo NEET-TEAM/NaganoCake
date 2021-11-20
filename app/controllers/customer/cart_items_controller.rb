2931472af0c8b7df7c5d9faa7a657eb66496fae6
sekai5108 <sekai.5108@gmail.com>
2021-11-20 13:02:27 +0000

    Add cart-indexの詳細作成　saveできない為作成途中

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,42 +1,53 @@
 class Customer::CartItemsController < ApplicationController
 
+  before_action :authenticate_customer!
+
   def index
     @cart_items = current_customer.cart_items
   
   end
   
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    
-    item_id = params[:cart_item][:item_id]
-    quantity = params[:cart_item][:quantity]
-    cart_item = current_customer.cart_items.find_by(item_id: item_id)
-    
-    if cart_item.present?
-      cart_item.quantity = quantity.to_i
-      cart_item.save
+    @cart_item = @customer.cart_item.new(cart_item_params)
+    @current_item = CartItem.find_by(item_id: @cart_item.item_id, customer_id: 
+    @cart_item.customer_id)
+    # カートに同じ商品がなければ新規追加、あれば既存のデータと合算
+    if @current_item.blank?
+      @cart_item.save
       redirect_to cart_items_path
-      
-    elsif @cart_item.save
-      redirect_to cart_items_path
-      
     else
-      @item = Item.find_by(id:@cart_item.item_id)
-      @genres = Genre.all
-      @cart_items = CartItem.new
-      
-      redirect_to customer_items_path
-      
+      @current_item.quantity += params[:quantity].to_i
+      new_quantity = @cart_item.quantity + @current_item.quantity
+      @current_item.update(quantity: new_quantity)
+      redirect_to cart_items_path
+    end
+  end
+  
+  def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to request.referer
+    else
+      redirect_to request.referer
     end
   end
   
   def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
   end
+  
+  def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to request.referer
+  end
+  
+  
   
   private
 
   def cart_item_params
-    params.permit(:item_id, :quantity)
+    params.permit(:item_id, :quantity, :customer_id)
   end
 end
