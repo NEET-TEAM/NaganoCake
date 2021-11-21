522594b15fdc12f11fb325fff3db0d075bb881ba
sekai5108 <sekai.5108@gmail.com>
2021-11-21 10:13:39 +0000

    Add cart-itemsのインデックスの詳細作りこみ

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -10,9 +10,25 @@ class Customer::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    @cart_item.save
     
-    redirect_to customer_cart_items_path
+    item_id = params[:cart_item][:item_id]
+    quantity = params[:cart_item][:quantity]
+    cart_item = current_customer.cart_items.find_by(item_id: item_id)
+    
+    if cart_item.present?
+      cart_item.quantity = quantity.to_i
+      cart_item.save
+      redirect_to customer_cart_items_path
+      
+    elsif
+      @cart_item.save
+        
+    else 
+      @item = Item.find_by(id:@cart_item.item.id)
+      @gernes = Genre.all
+      @cart_items = CartItem.new
+      redirect_to customer_items_path
+    end
   end
   
   def update
