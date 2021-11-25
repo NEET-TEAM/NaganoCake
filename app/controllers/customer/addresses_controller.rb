291ba060fdac4d1a6441d433512e75a8291a960c
sanotakumi <t.sano.19941118@gmail.com>
2021-11-25 06:53:34 +0000

    修正

--- app/controllers/customer/addresses_controller.rb
+++ app/controllers/customer/addresses_controller.rb
@@ -1,16 +1,16 @@
 class Customer::AddressesController < ApplicationController
 
-  
+
   def new
     @address = Address.new
-    @addresses = Address.all
+    @addresses = current_customer.addresses
   end
-    
+
   def index
     @address = Address.new
-    @addresses = Address.all
+    @addresses = current_customer.addresses
   end
-  
+
   def create
     address = Address.new(address_params)
     address.customer_id = current_customer.id
@@ -20,12 +20,12 @@ class Customer::AddressesController < ApplicationController
       redirect_to customer_items_path
     end
   end
-  
+
   def edit
     @address = Address.find(params[:id])
-    
+
   end
-  
+
   def update
     @address = Address.find(params[:id])
     if @address.update(address_params)
@@ -34,17 +34,17 @@ class Customer::AddressesController < ApplicationController
        redirect_to edit_customer_address_path
     end
   end
-  
+
   def destroy
     @address = Address.find(params[:id])
     @address.destroy
     redirect_to customer_addresses_path
   end
-  
+
   private
-  
+
   def address_params
     params.require(:address).permit(:address, :name, :postal_code)
   end
-    
+
 end
